9ad802973f771751cb790b32f612d39e22ce8e62
iamBharatManral <justbharatmanral@gmail.com>
2023-08-15 21:46:56 +0530

    feat: add specialized greeting for each user

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,43 @@
 use std::io::stdin;
 
+struct Visitor{
+    name: String,
+    greeting: String
+}
+
+impl Visitor{
+    fn new(name: &str, greeting: &str) -> Self{
+        Self{
+            name: name.to_owned(),
+            greeting: greeting.to_owned()
+        }
+    }
+    fn greet(&self){
+        println!("{}", self.greeting);
+    }
+}
+
 fn main() {
-    let visitors = ["bert", "steve", "fred"];
+    println!(":: 🌴 Welcome to the Treehouse 🌴 ::  🙏 ::");
+
+    let visitors = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi Steve. Your milk is in the fridge. 🥛"),
+        Visitor::new("fred", "Wow, who invited Fred? 🙂")
+    ];
 
     println!("Hello, what's your name? 🤷‍");
     let name = whats_your_name();
-    println!("Hello, {}! 👋", name);
 
-    let mut allow_them_in = false;
-    for visitor in &visitors {
-        if visitor == &name {
-            allow_them_in = true
-        }
+    let known_visitor = visitors
+        .iter()
+        .find(|visitor| visitor.name == name);
+
+    match known_visitor {
+        Some(name) => name.greet(),
+        None => println!("Sorry {}, you aren't on the list. 😞", name)
     }
 
-    if allow_them_in {
-        println!("🌴 Welcome to the Treehouse 🌴, {}! 🙏", name);
-    }else {
-        println!("Sorry, you aren't on the list. 😞");
-    }
 }
 
 fn whats_your_name() -> String{
