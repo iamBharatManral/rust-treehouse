0a573f7eaf45ace3e28f216658395a603893f5b6
iamBharatManral <justbharatmanral@gmail.com>
2023-08-15 19:44:51 +0530

    feat: user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, what's your name? 🤷‍");
+    let mut your_name = String::new();
+    stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read line 😔");
+    println!("Hello, {}", your_name);
 }
