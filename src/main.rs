19cb7fa15a6fb049ec0f3863766081b64935bd87
iamBharatManral <justbharatmanral@gmail.com>
2023-08-15 19:49:50 +0530

    bug: remove trailing newline from user input

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,16 @@ use std::io::stdin;
 
 fn main() {
     println!("Hello, what's your name? 🤷‍");
+    let name = whats_your_name();
+    println!("Hello, {}", name);
+}
+
+fn whats_your_name() -> String{
     let mut your_name = String::new();
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line 😔");
-    println!("Hello, {}", your_name);
+    your_name
+        .trim()
+        .to_lowercase()
 }
